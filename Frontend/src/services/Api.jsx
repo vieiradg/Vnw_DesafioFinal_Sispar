c4809ebf06dcff5012aeab4bdaf4812905cf2a94
vieiradg <diegorusty40@gmail.com>
2025-05-09 20:03:19 -0300

    updade
    add

--- Frontend/src/services/Api.jsx
+++ Frontend/src/services/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 //criando uma instancia no axios para facilitar a chamada da api
 const api = axios.create({
 
-    baseURL: "http://localhost:5000" // Define a url base para todas requisições feitas com essa instancia
+    baseURL: "https://vnw-back-end.onrender.com/colaborador/cadastrar" // Define a url base para todas requisições feitas com essa instancia
 })
 
 export default api;
